6ba249eaccfd39559c75657c2d403a69812fb3c6
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-02-07 19:14:21 +0300

    migrate

--- one_note/urls.py
+++ one_note/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 from document.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/', login_page, name='login'),
+    path('login/' , login_page, name='login'),
     path('register/', register_page, name='register'),
-    path('custom_logout_page/', custom_logout, name='logout'),
+    path('custom_logout/' ,custom_logout, name='logout'),
     path('', editor, name='editor'),
-    path('delete_document/<int:docid>/', delete_documents, name='delete'),
+    path('delete_document/<int:docid>/', delete_documents, name='delete_document'),
+    path('admin/', admin.site.urls),
 ]
