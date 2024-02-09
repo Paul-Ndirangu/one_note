1a41bea6397947c0873a9f5f2a52b583dacf01a7
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-02-09 09:56:18 +0300

    final commit

--- one_note/urls.py
+++ one_note/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('register/', register_page, name='register'),
     path('custom_logout/' ,custom_logout, name='logout'),
     path('', editor, name='editor'),
-    path('delete_document/<int:docid>/', delete_documents, name='delete_document'),
+    path('delete_document/<int:docid>/', delete_document, name='delete_document'),
     path('admin/', admin.site.urls),
 ]
